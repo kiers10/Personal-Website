3f355cff97a1418790f21fe9d01539e7f3e54429
kiers10 <densmore.kiersten@gmail.com>
2022-01-25 19:26:23 -0500

    Animations for projects

--- js/code.js
+++ js/code.js
@@ -36,5 +36,86 @@ function mobileNavCTA() {
 
         nav.style.display = "none";
     }
-
 }
+
+// Animations
+// Source: https://webdesign.tutsplus.com/tutorials/animate-on-scroll-with-javascript--cms-36671
+const scrollOffset = 100;
+
+const scrollElement = document.querySelector(".sutton-scroll");
+const scrollElement2 = document.querySelector(".majikk-scroll");
+const scrollElement3 = document.querySelector(".kindling-scroll");
+
+
+const elementInView = (el, offset = 0) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (
+        elementTop <= ((window.innerHeight || document.documentElement.clientHeight) - offset)
+    );
+};
+
+const displayScrollElement = () => {
+    scrollElement.classList.add('scrolled');
+}
+
+const displayScrollElement2 = () => {
+    scrollElement2.classList.add('scrolled');
+}
+
+const displayScrollElement3 = () => {
+    scrollElement3.classList.add('scrolled');
+}
+
+const hideScrollElement = () => {
+    scrollElement.classList.remove('scrolled');
+}
+
+const hideScrollElement2 = () => {
+    scrollElement2.classList.remove('scrolled');
+}
+
+const hideScrollElement3 = () => {
+    scrollElement3.classList.remove('scrolled');
+}
+
+const handleScrollAnimation = () => {
+    if (elementInView(scrollElement, scrollOffset)) {
+        displayScrollElement();
+    } else {
+        hideScrollElement();
+    }
+
+    if (elementInView(scrollElement2, scrollOffset)) {
+        displayScrollElement2();
+    } else {
+        hideScrollElement2();
+    }
+
+    if (elementInView(scrollElement3, scrollOffset)) {
+        displayScrollElement3();
+    } else {
+        hideScrollElement3();
+    }
+}
+
+//initialize throttleTimer as false
+let throttleTimer = false;
+
+const throttle = (callback, time) => {
+    //don't run the function while throttle timer is true
+    if (throttleTimer) return;
+    
+    //first set throttle timer to true so the function doesn't run
+    throttleTimer = true;
+    
+    setTimeout(() => {
+        //call the callback function in the setTimeout and set the throttle timer to false after the indicated time has passed 
+        callback();
+        throttleTimer = false;
+	}, time);
+}
+
+window.addEventListener('scroll', () => {
+    throttle(handleScrollAnimation, 250);
+})
